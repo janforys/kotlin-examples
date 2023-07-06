36a997e2a1a112b499474c77450962c573206831
janforys <janforys@wp.pl>
2023-07-06 02:39:27 +0200

    Create add-two-integers.kt

---
+++ intro/add-two-integers.kt
@@ -0,0 +1,13 @@
+/* https://ideone.com/eeK0Pz */
+
+fun main() {
+
+   //val enteredNumber1 = readLine()
+   val number1: Int = 11//enteredNumber1
+   
+   //val enteredNumber2 = readLine()
+   val number2: Int = 22//enteredNumber2
+   
+   val sum = number1 + number2
+   print("Sum is: $sum")
+}
